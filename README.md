0412929638e2bedd691c4e35b91e553a26d00915
montsieur <dndott@gmail.com>
2024-06-18 21:16:57 +1000

    learning inheritance and polymorphism

--- README.md
+++ README.md
@@ -9,3 +9,17 @@
 ## Abstraction
 - Concept of hiding the complex implementation details and showing only the necessary features of an object. 
 - Achieved by using Abstract class and method.
+
+### Abstraction Class and Method
+-
+
+
+## Inheritance
+- Mechanism of creating a new class based on an exisiting class, by inheriting properties (attributes and/or methods)
+- Parent class: The class from where the features are inherited from.
+- Child class: The class that inherits the features.
+- Benefits: Classes are created easily, easy maintenance.
+
+## Polymorphism
+- Ability to present the same interface for different data types.
+- Allows methods to do diferrent things based on the object it is acting upon.
---
+++ inheritance.py
@@ -0,0 +1,49 @@
+class Vehicle:
+    def __init__ (self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        return f"The engine of the {self.model} is starting."
+
+    def stop_engine(self):
+        return f"The engine of the {self.model} is stopping."
+
+    def display_info(self):
+        return f"The vehicle is Brand: {self.brand}, Model: {self.model}, Year: {self.year}."
+    
+class Car(Vehicle):
+    def __init__(self, brand, model, year, num_doors):
+        super().__init__(brand, model, year) # Inheriting from Vehicle Class
+        self.num_doors = num_doors
+
+    def open_trunk(self):
+        return f"The trunk of the {self.model} is open."
+
+    # Overriding display_info method   
+    def display_info(self):
+        return f"The vehicle is Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Doors: {self.num_doors}"
+
+class Bicycle(Vehicle):
+    def __init__(self, brand, model, year, type_bike):
+        super().__init__(brand, model, year) # Inherits from Vehicle Class
+        self.type_bike = type_bike
+
+    def ring_bell(self):
+        return f"Ringing the bell on {self.model}."
+    
+    # Overriding display_info method   
+    def display_info(self):
+        return f"The bicycle is Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Type: {self.type_bike}"
+    
+my_car = Car("Toyota", "Corolla", 2021, 4)
+my_bicycle = Bicycle("Giant", "Escape 3", 2022, "Road Bike")
+
+print(my_car.start_engine())
+print(my_bicycle.ring_bell())
+    
+print(my_car.display_info())
+print(my_bicycle.display_info())
+
+    
